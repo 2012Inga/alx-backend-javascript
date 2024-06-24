b75a1f1e5be13c9c7232b864cbd1dc8fec68cb80
2012Inga <inganathi2001@gmail.com>
2024-06-24 13:36:14 +0200

    changes

--- 0x00-ES6_basic/4-main.js
+++
@@ -1,4 +0,0 @@
-import returnHowManyArguments from './4-rest-parameter.js';
-
-console.log(returnHowManyArguments("one"));                 // Output: 1
-console.log(returnHowManyArguments("one", "two", 3, "4th")); // Output: 4
--- 0x00-ES6_basic/5-main.js
+++
@@ -1,4 +0,0 @@
-import concatArrays from './5-spread-operator.js';
-
-console.log(concatArrays(['a', 'b'], ['c', 'd'], 'Hello'));
-// Output: ['a', 'b', 'c', 'd', 'H', 'e', 'l', 'l', 'o']
